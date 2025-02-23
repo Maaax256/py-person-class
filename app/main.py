e26e6f3f0804f44b9ed48e651bd2f81d73f6eb2f
Maksym Omelkov <omelkov85@gmail.com>
2025-02-23 18:06:11 +0200

    Returning list with Person instances instead of dicts (1.1)

--- app/main.py
+++ app/main.py
@@ -5,29 +5,24 @@ class Person:
         self.name = name
         self.age = age
         Person.people[name] = self
-    pass
 
 
 def create_person_list(people: list[dict]) -> list[Person]:
-    person_list = []
-
-    for person_dict in people:
-        person_list.append(
-            Person(
-                person_dict["name"],
-                person_dict["age"],
-            )
-        )
+    person_list = [
+        Person(person_dict["name"], person_dict["age"])
+        for person_dict in people
+    ]
 
     for person_dict in people:
         name = person_dict["name"]
         person = Person.people[name]
-        if "wife" in person_dict and person_dict["wife"] is not None:
+        # if "wife" in person_dict and person_dict["wife"] is not None:
+        if person_dict.get("wife") is not None:
             wife_name = person_dict["wife"]
             person.wife = Person.people[wife_name]
-        elif "husband" in person_dict and person_dict["husband"] is not None:
+        # elif "husband" in person_dict and person_dict["husband"] is not None:
+        elif person_dict.get("husband") is not None:
             husband_name = person_dict["husband"]
             person.husband = Person.people[husband_name]
 
     return person_list
-    pass
